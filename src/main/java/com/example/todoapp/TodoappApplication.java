3f3a13161e1911dee26d3e9ad9b872c5e0fd26b7
Sidar Orman <sidarormang@gmail.com>
2025-11-14 11:58:49 +0300

    t

--- src/main/java/com/example/todoapp/TodoappApplication.java
+++ src/main/java/com/example/todoapp/TodoappApplication.java
@@ -2,6 +2,7 @@ package com.example.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,6 +13,7 @@ import static org.springframework.data.web.config.EnableSpringDataWebSupport.Pag
 @EnableFeignClients
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 @EnableScheduling
+@EnableDiscoveryClient
 public class TodoappApplication {
 
 	public static void main(String[] args) {
